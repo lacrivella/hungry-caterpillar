db16ebe170e375e6e19a26238f9d2b56aaf66802
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-25 21:28:35 -0700

    wired up fruit bar

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-console.log('hoi hoi');
-
 //create var that selects all fruits
 const fruitBars = document.querySelectorAll('.fruit');
 
 //add event listener
 for(let i = 0; i < fruitBars.length; i++) {
     const fruitBar = fruitBars[i];
+    fruitBar.addEventListener('click', () => {
+    });
 }
+console.log(fruitBars);
