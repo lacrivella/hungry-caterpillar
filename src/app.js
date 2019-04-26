3060277492a716808a40a4587b7c6cca9ef4025d
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-26 09:08:54 -0700

    called the function, and it works

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 //create var that selects all fruits
 import feedNyan from './feed-nyan.js';
 const fruitBars = document.querySelectorAll('.fruit');
-
+const cat = document.getElementById('cat');
 //add event listener
 for(let i = 0; i < fruitBars.length; i++) {
     const fruitBar = fruitBars[i];
     fruitBar.addEventListener('click', () => {
-        console.log(fruitBar.value);
+
+        feedNyan(fruitBar.value, cat);
     });
 }
--- test/tests.js
+++ test/tests.js
@@ -1,20 +1,7 @@
 import feedNyan from '../src/feed-nyan.js';
 const test = QUnit.test;
 
-//function
-// function feedNyan(foodName, parentElement) {
-//     const span = document.createElement('span');
-//     span.classList.add('part', foodName);
-
-//     parentElement.appendChild(span);
-//     return parentElement.innerHTML;
-// }
-
 test('test adding watermelon to nyancat', function(assert) {
-
-//I have to do something with a parent element and innerHTML according to notes
-//I need to use parentElement.appendChild() according to notes
-
     //Arrange
     // Set up your parameters and expectations
     const foodName = 'watermelon';
