b627f0d91107e403cc1cfa466b2e68b9be8873aa
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-25 21:21:17 -0700

    added app.js file and linked to index.html Tested in console

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log('hoi hoi');
+
+//create var that selects all fruits
+const fruitBars = document.querySelectorAll('.fruit');
+
+//add event listener
+for(let i = 0; i < fruitBars.length; i++) {
+    const fruitBar = fruitBars[i];
+}
