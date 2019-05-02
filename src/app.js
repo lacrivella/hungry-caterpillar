be6e49b57341f8ec2b097a3abc818548ef899976
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-01 20:50:36 -0700

    created function that selects all bars and removes power class

--- src/app.js
+++ src/app.js
@@ -13,5 +13,25 @@ for(let i = 0; i < fruitBars.length; i++) {
 
 //need to connect gemBar with event listener
 //will need to do a doc.querySelectorAll(.dance) and then do a remove all?
+const powerUp = document.querySelectorAll('.color');
+
 //will need to make function removeAllDancing with variable for dance, 
+for(let i = 0; i < powerUp.length; i++) {
+    const powerButton = powerUp[i];
+    powerButton.addEventListener('click', () => {
+        removeAllPower();
+        addPower(powerUp.value);
+    });
+}
+function addPower() {
+    
+}
+function removeAllPower() {
+    const barAll = document.querySelectorAll('span');
+    for(let i = 0; i < barAll.length; i++) {
+        const bar = barAll[i];
+        bar.classList.remove('power');
+    }
+}
+
 //make another function to add dancing parts? doc.queryselectorall that selects all of the corr. colors
